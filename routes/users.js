ccca5e9ddc44ab85d063500d03b398ae80843d32
Luke-Quarderer <lquarderer13@gmail.com>
2023-08-28 19:40:49 +1000

    Aug 28

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/user')
 
 // All Users Route
 router.get('/', (req, res) => {
@@ -8,12 +9,19 @@ router.get('/', (req, res) => {
 
 // New User Route
 router.get('/new', (req, res) => {
-    res.render('users/new')
+    res.render('users/new', {user: new User()})
 }) 
 
 // Create User Route
 router.post('/', (req, res) => {
-    res.send('Create')
-}) 
+    const user = new User({
+        name: req.body.name
+    })
+    user.save().then((err, newUser) => {
+        res.redirect(`users`)
+    }).catch((err)=>{
+        console.log(err)
+    }) 
+})
 
 module.exports = router  
